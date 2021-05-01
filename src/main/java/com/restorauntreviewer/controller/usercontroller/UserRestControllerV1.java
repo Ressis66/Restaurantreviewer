3530ba0fa0efb05ae2544e63eee52b6e7db361a5
Ressis66 <vaganov.aleksandr.88@mail.ru>
2021-05-01 08:49:17 +0500

    Task 02

--- src/main/java/com/restorauntreviewer/controller/usercontroller/UserRestControllerV1.java
+++ src/main/java/com/restorauntreviewer/controller/usercontroller/UserRestControllerV1.java
@@ -13,12 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * REST controller user connected requestst.
- *
- * @author Eugene Suleimanov
- * @version 1.0
- */
+
 
 @RestController
 @RequestMapping(value = "/api/v1/users/")
