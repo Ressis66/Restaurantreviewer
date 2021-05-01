70f55b39bf834a9fd340bc86a1a4d557d444914c
Ressis66 <vaganov.aleksandr.88@mail.ru>
2021-05-01 19:14:12 +0500

    Task 06

--- src/main/java/com/restorauntreviewer/controller/usercontroller/UserRegistrationController.java
+++ src/main/java/com/restorauntreviewer/controller/usercontroller/UserRegistrationController.java
@@ -1,8 +1,11 @@
 package com.restorauntreviewer.controller.usercontroller;
 
+import com.restorauntreviewer.model.Dish;
 import com.restorauntreviewer.model.User;
 import com.restorauntreviewer.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,8 +22,8 @@ public class UserRegistrationController {
     }
 
     @PostMapping
-    public User registerUserAccount(@RequestBody User user) {
-        userService.register(user);
-        return user;
+    public ResponseEntity<User> registerUserAccount(@RequestBody User user) {
+        User created = userService.register(user);
+        return new ResponseEntity<>(created,  HttpStatus.CREATED);
     }
 }
