b77aaba7a2db0e27d0572c42f68bc7f9539e34a5
Ressis66 <vaganov.aleksandr.88@mail.ru>
2021-05-17 00:28:22 +0500

    Changing BaseEntity according to review.

--- src/main/java/com/restorauntreviewer/model/BaseEntity.java
+++ src/main/java/com/restorauntreviewer/model/BaseEntity.java
@@ -74,4 +74,16 @@ public class BaseEntity  {
         this.status = status;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
